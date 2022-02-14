c679ad53fb6a40c9a1bbd729a19bbaffcb194c46
DouglasASillex <4769745600@GSA.GOV>
2022-02-14 06:52:57 -0500

    inital ci

--- tests/variables.tf
+++ tests/variables.tf
@@ -1,6 +1,12 @@
-variable "prisma_secrets_name" {
+variable account_id {
   type        = string
-  description = "The AWS Account Id where prisma cloud is located"
+  description = "The AWS Account Id where prisma cloud monitor is hosted"
+  default     = "188619942792"
+}
+variable external_id {
+  type        = string
+  description = "External ID of the entity that is allowed to assume this role"
+  default     = "622cbd5e-f964-4187-918f-33aed90f49ac"
 }
 
 variable "is_read_only" {
